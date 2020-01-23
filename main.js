e2da9b960365dd64892c3f626e42d2788c7a15fc
Alexander Rowland <arowland900@gmail.com>
2020-01-22 22:20:48 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// var iframe = document.createElement("iframe");
+// iframe.setAttribute('id', "ifrm1");
+// iframe.setAttribute('src', 'files/IntakeSheet.pdf'); // change the URL
+// iframe.setAttribute('width', '100%');
+// iframe.setAttribute('height', '500');
+// iframe.setAttribute('frameBorder', '0');
+// iframe.setAttribute('scrolling', 'no');
+// iframe.setAttribute('onload' ,"setIframeHeight(this.id)");
+// document.body.appendChild(iframe);
+
