fb6331636d09b191ca460223a232074b662ad16b
Alexander Rowland <arowland900@gmail.com>
2020-01-23 14:53:58 -0800

    add other.html to implement pdf.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// var iframe = document.createElement("iframe");
-// iframe.setAttribute('id', "ifrm1");
-// iframe.setAttribute('src', 'files/IntakeSheet.pdf'); // change the URL
-// iframe.setAttribute('width', '100%');
-// iframe.setAttribute('height', '500');
-// iframe.setAttribute('frameBorder', '0');
-// iframe.setAttribute('scrolling', 'no');
-// iframe.setAttribute('onload' ,"setIframeHeight(this.id)");
-// document.body.appendChild(iframe);
-let idx = 0
 const next = document.getElementById('next')
 const c1 = document.querySelector(`.c1`)
 const c2 = document.querySelector(`.c2`)
@@ -15,15 +5,28 @@ const c3 = document.querySelector(`.c3`)
 const c4 = document.querySelector(`.c4`)
 const c5 = document.querySelector(`.c5`)
 const c6 = document.querySelector(`.c6`)
+const containers = document.querySelectorAll('.container')
+
+let idx = 1
 let arr = [c1, c2, c3,c4,c5,c6]
 
-next.addEventListener('click', function (e) {
+containers.forEach((e,i) => { if(i )e.style.display = 'none' })
+
+for(let j = 0; j < arr.length; j++){
+    console.log('hit!')
+    hideDisplay()
+}
+
+
+next.addEventListener('click', hideDisplay)
+
+function hideDisplay(e){
     for(let i = 0; i < arr.length; i++){
-        if(i == idx){
-            document.querySelector(`.c${i+1}`).style.display = 'block'
-        } else {
-            document.querySelector(`.c${i+1}`).style.display = 'none'
-        }
+        console.log("inner hit!")
+        i == idx 
+        ? document.querySelector(`.c${i+1}`).style.display = 'block'
+        : document.querySelector(`.c${i+1}`).style.display = 'none'
     }
     idx = (idx + 1) % 6
-})
+}
+
